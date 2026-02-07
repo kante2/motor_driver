796788f48e8e6fc7b3d9290bd2a1cae08fb25dd9
kante2 <gangtae9050@gmail.com>
2026-02-07 13:47:58 +0900

    fix folder direction

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 set -e
 
 # chmod
-chmod +x /home/autonav/KAIST_Mobility_Challenge_SDK/build.sh
+# chmod +x /home/autonav/KAIST_Mobility_Challenge_SDK/build.sh
+chmod +x ~/KAIST_MOBILITY_CHALLENGE_SDK/build.sh
 
 # ROS2 환경 설정
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 # Driver_ROS2 패키지 빌드
-cd ~/KAIST_Mobility_Challenge_SDK/examples/Driver_ROS2
+# cd ~/KAIST_Mobility_Challenge_SDK/examples/Driver_ROS2
+cd ~/KAIST_MOBILITY_CHALLENGE_SDK/examples/Driver_ROS2
 
 colcon build --symlink-install
 source install/setup.bash
