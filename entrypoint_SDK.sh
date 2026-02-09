d7c71f3dd18502cf0a586278120960aabdd1d357
kante2 <gangtae9050@gmail.com>
2026-02-09 21:01:33 +0900

    fix entrypooint  direction,,

--- entrypoint_SDK.sh
+++ entrypoint_SDK.sh
@@ -10,7 +10,7 @@ echo "CAV_ID: $CAV_ID, PROBLEM_ID: $PROBLEM_ID, ROLE: $ROLE" >&2
 
 # ROS2 환경 설정
 source /opt/ros/$ROS_DISTRO/setup.bash
-cd ~/KAIST_MOBILITY_CHALLENGE_SDK/examples/Driver_ROS2
+cd ~/motor_driver/examples/Driver_ROS2
 source install/setup.bash
 export ROS_LOCALHOST_ONLY=0
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
