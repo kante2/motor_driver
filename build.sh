d2f6486679943dd3dafefae403bb667c3a110cf7
kante2 <gangtae9050@gmail.com>
2026-02-09 20:55:43 +0900

    Update paths from KAIST_MOBILITY_CHALLENGE_SDK to motor_driver

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-cd ~/KAIST_MOBILITY_CHALLENGE_SDK
+cd ~/motor_driver
 chmod +x *
 
 # ROS2 환경 설정
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 # Driver_ROS2 패키지 빌드
-# cd ~/KAIST_Mobility_Challenge_SDK/examples/Driver_ROS2
+# cd ~/motor_driver/examples/Driver_ROS2
 
-cd ~/KAIST_MOBILITY_CHALLENGE_SDK/examples/Driver_ROS2
+cd ~/motor_driver/examples/Driver_ROS2
 rm -rf build install log
 colcon build --symlink-install
 
